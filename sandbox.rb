2e17bb5f2bb2e048da53e64784156f835dede6da
Tarannum (Tara) Lakdawala <40361282+TaraLakdawala@users.noreply.github.com>
2018-07-02 11:50:53 -0400

    Create sandbox.rb

---
+++ sandbox.rb
@@ -0,0 +1,14 @@
+puts "This is a pretty cool project"
+
+class User
+def initialize(name, username, password)
+@name = name
+@username = username
+@password = password
+end
+
+def takePicture
+puts "Take a picture!"
+end
+
+end
